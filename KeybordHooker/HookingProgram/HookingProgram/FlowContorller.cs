ac398f7cc9e34785fd648a3530375e94d2f7d14a
Yongdae-Kim <yongdae91@hanmail.net>
2014-07-04 17:41:06 +0900

    first commit

---
+++ KeybordHooker/HookingProgram/HookingProgram/FlowContorller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace HookingProgram
+{
+    class FlowContorller
+    {
+        private Hashtable keyTable;
+        private String result;
+
+        public void process(String key)
+        {
+            keyTable = new KeyTableMaker().getKeyTable();
+
+            keySearcher searcher = new keySearcher(key, keyTable);
+            result = searcher.getKeyString();
+            Console.WriteLine(result);
+        }
+    }
+}
