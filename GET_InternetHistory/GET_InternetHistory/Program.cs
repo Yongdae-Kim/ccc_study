1e3bba7d4efe89cdf9797b8d4250f169c78b7bad
Yongdae-Kim <yongdae91@hanmail.net>
2014-08-05 20:50:28 +0900

    ccc project

---
+++ GET_InternetHistory/GET_InternetHistory/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+
+namespace GET_InternetHistory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String userName = Environment.UserName;
+
+            SQLiteConnection conn = new SQLiteConnection
+            ("Data Source=C:\\Users\\" + userName + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History");
+            conn.Open();
+
+            SQLiteCommand cmd = new SQLiteCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "Select * From urls";
+
+            SQLiteDataReader dr = cmd.ExecuteReader();
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter("C:\\History.txt");
+            
+            while (dr.Read())
+                file.WriteLine(dr[1].ToString());
+            
+            file.Close();
+        }
+    }
+}
---
+++ HookingProgram/HookingProgram/keySearcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace HookingProgram
+{
+    class keySearcher
+    {
+        private String key;
+        private Hashtable keyTable;
+     
+        public keySearcher(String key, Hashtable keyTable)
+        {
+            this.key = key;
+            this.keyTable = keyTable;
+        }
+
+        public String getKeyString()
+        {
+            if (keyTable.ContainsKey(key))
+                return keyTable[key].ToString();
+            else
+                return key;
+        }
+    }
+}
